d153b463c5e4a211400cc9b8a455d605654375e9
Leon Welchert <lichturm@posteo.de>
2025-10-26 09:48:49 +0100

    update vector DB password name in python server runscript

--- python-server/server-runscript.sh
+++ python-server/server-runscript.sh
@@ -4,7 +4,7 @@ PG_HOST=vectordb.bibliophage.irminsul
 PG_PORT=5432
 PG_DB_NAME=vectors
 PG_USER=bibliophage
-PG_PASSWORD=$(kubectl -n bibliophage get secret bibliophage-app-user -o jsonpath='{.data.password}' | base64 -d)
+PG_PASSWORD=$(kubectl -n bibliophage get secret db-standard-user -o jsonpath='{.data.password}' | base64 -d)
 
 # when we use a "normal" connection string, sqlalchemy may fall back to psycopg2 instead of psycopg3
 #export PG_CONNECTION_STRING="postgresql://${PG_USER}:${PG_PASSWORD}@${PG_HOST}:${PG_PORT}/${PG_DB_NAME}"
