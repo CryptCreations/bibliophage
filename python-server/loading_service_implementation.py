d094bdab44d14be3dc0a92996e345939dc331c3e
Leon Welchert <lichturm@posteo.de>
2025-11-19 19:04:51 +0100

    correctly handle pdf load requests

--- python-server/loading_service_implementation.py
+++ python-server/loading_service_implementation.py
@@ -2,6 +2,9 @@ from bibliophage.v1alpha1.pdf_connect import LoadingService
 import bibliophage.v1alpha1.pdf_pb2 as api
 
 import logging
+import traceback
+import inspect
+
 import os
 import sys
 from tempfile import NamedTemporaryFile
@@ -34,64 +37,69 @@ class LoadingServiceImplementation:
 
 
     async def load_p_d_f(self, request: api.PdfLoadRequest, ctx):
+        try:
+            # TODO: actually do stuff with the request
+            logger.info(f"Received LoadPdf request for file: {request.pdf_name}")
 
-        # TODO: actually do stuff with the request
-        logger.info(f"Received LoadPdf request for file: {request.pdf_name}")
+            # request will be the PdfLoadRequest from the client
+            # we should access the fields in the request and do stuff with it
+            # like setting metadata
 
-        # request will be the PdfLoadRequest from the client
-        # we should access the fields in the request and do stuff with it
-        # like setting metadata
-
-        pdf_bytes = request.file_data
-        
-        # store transmitted data in a temporary file
-        # `with` will always run the cleanup functions of the file object we create
-        # https://docs.python.org/3/reference/compound_stmts.html#index-18
-        with NamedTemporaryFile(delete=True, suffix=".pdf") as tmp:
-            tmp.write(pdf_bytes)
-            tmp.flush()
-            logger.info(f'Temporary file information:\n {os.stat(tmp.name)}')
+            pdf_bytes = request.file_data
+            
+            # store transmitted data in a temporary file
+            # `with` will always run the cleanup functions of the file object we create
+            # https://docs.python.org/3/reference/compound_stmts.html#index-18
+            # https://docs.python.org/3/reference/datamodel.html#with-statement-context-managers
+            with NamedTemporaryFile(delete=True, suffix=".pdf") as tmp:
+                tmp.write(pdf_bytes)
+                tmp.flush()
+                logger.info(f'Temporary file name: {tmp.name}')
 
 
-            # https://python.langchain.com/api_reference/community/document_loaders/langchain_community.document_loaders.pdf.PyPDFLoader.html
+                # https://python.langchain.com/api_reference/community/document_loaders/langchain_community.document_loaders.pdf.PyPDFLoader.html
 
-            # access path on disk and try to  load
-            #loader = PyPDFLoader(request.pdf_origin_path)
-            # use transferred data in request
-            loader = PyPDFLoader(tmp.name)
+                # access path on disk and try to  load
+                #loader = PyPDFLoader(request.pdf_origin_path)
+                # use transferred data in request
+                loader = PyPDFLoader(tmp.name)
+                documents = loader.load()
 
-        # as far as i know, this loads individual pages
-        documents = loader.load()
-        logger.info(f'PDF "documents" loaded: {len(documents)}')
+            # as far as i know, this loads individual pages
+            logger.info(f'PDF "documents" loaded: {len(documents)}')
 
-        chunk_size = request.chunk_size if request.HasField('chunk_size') else 600
-        chunk_overlap = request.chunk_overlap if request.HasField('chunk_overlap') else 50
-        # https://python.langchain.com/api_reference/text_splitters/character/langchain_text_splitters.character.RecursiveCharacterTextSplitter.html
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=chunk_size, chunk_overlap=chunk_overlap
-        )
-        chunks = text_splitter.split_documents(documents)
+            chunk_size = request.chunk_size if request.HasField('chunk_size') else 600
+            chunk_overlap = request.chunk_overlap if request.HasField('chunk_overlap') else 50
+            # https://python.langchain.com/api_reference/text_splitters/character/langchain_text_splitters.character.RecursiveCharacterTextSplitter.html
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size=chunk_size, chunk_overlap=chunk_overlap
+            )
+            # TODO: perhaps we can send some kind of progress indicator to the user here?
+            chunks = text_splitter.split_documents(documents)
 
-        # it seems weird to attach information like the original PDF path to  every chunk
-        # TODO: we probably just want to have a separate, regular table containing the origin information and
-        # then we can reference that table
-        for chunk in chunks:
-            chunk.metadata.update({"source": request.pdf_origin_path})
-            chunk.metadata.update({"origin_path": request.pdf_origin_path})
-            chunk.metadata.update({"system": request.pdf_system})
-            chunk.metadata.update({"type": request.pdf_type})
-            chunk.metadata.update({"page_count": len(documents)})
-            chunk.metadata.update({"chunk_count": len(chunks)})
-            # TODO: uuid/md5sum
-        
-        # Store Chunks in Vector DB
-        pgvector.add_documents(chunks)
+            # it seems weird to attach information like the original PDF path to  every chunk
+            # TODO: we probably just want to have a separate, regular table containing the origin information and
+            # then we can reference that table
+            for chunk in chunks:
+                chunk.metadata.update({"source": request.pdf_origin_path})
+                chunk.metadata.update({"origin_path": request.pdf_origin_path})
+                chunk.metadata.update({"system": request.pdf_system})
+                chunk.metadata.update({"type": request.pdf_type})
+                chunk.metadata.update({"page_count": len(documents)})
+                chunk.metadata.update({"chunk_count": len(chunks)})
+                # TODO: uuid/md5sum
+            
+            # Store Chunks in Vector DB
+            self.pgvector.add_documents(chunks)
 
-        # when that's done, we return a PdfLoadResponse
-        return api_pb2.PdfLoadResponse(
-            success=True,
-            message=f"PDF {request.pdf_name} loaded successfully",
-            chunks_created=len(chunks),
-            # TODO: uuid/md5sum
-            # document_id="some-uuid"
-        )
+            # when that's done, we return a PdfLoadResponse
+            return api.PdfLoadResponse(
+                success=True,
+                message=f"PDF {request.pdf_name} loaded successfully",
+                chunks_created=len(chunks),
+                # TODO: uuid/md5sum
+                # document_id="some-uuid"
+            )
+        except Exception as e:
+            logger.error(f"Exception: {e}\n{traceback.format_exc()}")
+            raise
