0b3be6acd28be407a07d02f92d227bde50cb56af
Leon Welchert <lichturm@posteo.de>
2025-11-22 03:22:04 +0100

    Update to tailwindcss 4.1.17 and daisyui 5.5.5

--- web-ui/vite.config.ts
+++ web-ui/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from "@tailwindcss/vite";
 
 // this plugin tells typescript to treat any imported .vue files as DefineComponent
 // (Typescript wants types for everything, hence the name)
@@ -8,9 +9,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),
-  vueDevTools({
-    launchEditor: 'codium'
-  }),
-],
+  plugins: [
+    vue(),
+    vueDevTools({
+      launchEditor: 'codium'
+    }),
+    tailwindcss(),
+  ],
 })
